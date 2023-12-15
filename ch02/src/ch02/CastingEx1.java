aefb297909b04fd88f70e537e1af8feaffa5a401
Wooya <wooyya03@naver.com>
2023-12-16 03:31:08 +0900

    2-12

---
+++ ch02/src/ch02/CastingEx1.java
@@ -0,0 +1,11 @@
+package ch02;
+// 예제 2-12
+public class CastingEx1 {
+	public static void main(String[] args) {
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score="+score);
+		System.out.println("d="+d);
+	}
+}

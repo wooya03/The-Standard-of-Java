706f915477e45a6a77b68fa4398af98bb6c351af
Wooya <wooyya03@naver.com>
2023-12-16 23:43:26 +0900

    3-2

---
+++ ch03/src/ch03/OperatorEx2.java
@@ -0,0 +1,15 @@
+package ch03;
+// 예제 3-2
+public class OperatorEx2 {
+	public static void main(String[] args) {
+		int i = 5, j = 0;
+		
+		j = i++;
+		System.out.println("j=i++; 실행후, i=" + i + ", j=" + j);
+		
+		i = 5;
+		j = 0;
+		j = ++i;
+		System.out.println("j=i++; 실행후, i=" + i + ", j=" + j);
+	}
+}

91bbdc48f6a29830305cbc68f9036e5f494eac32
Wooya <wooyya03@naver.com>
2023-12-16 23:43:17 +0900

    3-1

---
+++ ch03/src/ch03/OperatorEx1.java
@@ -0,0 +1,15 @@
+package ch03;
+// 예제 3-1
+public class OperatorEx1 {
+	public static void main(String[] args) {
+		int i = 5;
+		i++;
+		System.out.println(i);
+		
+		i = 5;
+		++i;
+		System.out.println(i);
+		
+		// 수식이나 메서드 호출에 포함되지 않고 독립적인 하나의 문장으로 쓰인 경우에는 차이가 없음
+	}
+}

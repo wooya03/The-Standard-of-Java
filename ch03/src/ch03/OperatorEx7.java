a89a15b924bd38c9ec4ac05ae4e61388f4d9fb3e
Wooya <wooyya03@naver.com>
2023-12-16 23:43:47 +0900

    3-7

---
+++ ch03/src/ch03/OperatorEx7.java
@@ -0,0 +1,10 @@
+package ch03;
+// 예제 3-7
+public class OperatorEx7 {
+	public static void main(String[] args) {
+		byte a = 10;
+		byte b = 30;
+		byte c = (byte)(a * b);
+		System.out.println(c);
+	}
+}

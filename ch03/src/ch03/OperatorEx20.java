b72c88eecfe0b3af87a5fb4284ddf3450b60eb0c
Wooya <wooyya03@naver.com>
2023-12-17 00:01:45 +0900

    3-20

---
+++ ch03/src/ch03/OperatorEx20.java
@@ -0,0 +1,9 @@
+package ch03;
+// 예제 3-20
+public class OperatorEx20 {
+	public static void main(String[] args) {
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+	}
+}
